ce2ec269617637dd41e31bd3f50ca5b93f10dddf
chrishahn <chrishahn97@outlookcom>
2023-10-17 18:18:13 -0500

    added Layout and Sidebat components

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,17 @@
+import './index.scss'
+import { Link } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt='Logo' />
+            <img src={LogoSubtitle} alt='sloboan' />
+        </Link>
+    </div>
+)
+
+
+export default Sidebar
